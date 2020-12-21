12aed76d23cc4e3e27da95c2e0a365b22c9a4813
Derek Hsieh <devderekhsieh@gmail.com>
2020-12-20 21:56:00 -0800

    Initial Commit

---
+++ Weathery/WeatheryApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatheryApp.swift
+//  Weathery
+//
+//  Created by Derek Hsieh on 12/20/20.
+//
+
+import SwiftUI
+
+@main
+struct WeatheryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
