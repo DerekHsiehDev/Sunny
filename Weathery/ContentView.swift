204d2d17418de16cc544be22a7d8741ea7937f41
Derek Hsieh <devderekhsieh@gmail.com>
2020-12-20 23:38:29 -0800

    initial commit

--- Weathery/ContentView.swift
+++ Weathery/ContentView.swift
@@ -7,10 +7,121 @@
 
 import SwiftUI
 
+class Cities {
+    
+    var city: String
+    var country: String
+    
+    init(city: String, country: String) {
+        self.city = city
+        self.country = country
+    }
+}
+
+let cities = [
+    "Corona",
+    "Taipei",
+    "Los Angeles",
+    "Palm Springs",
+    "Mumbai",
+    "Tokyo"
+]
+
+
+
 struct ContentView: View {
+    @State var addButtonTapped = false
+    @State var searchText = ""
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Color(#colorLiteral(red: 0.9371728301, green: 0.9373074174, blue: 0.9371433854, alpha: 1))
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                Spacer()
+                
+                if addButtonTapped {
+                    ZStack {
+                        VStack {
+                            RoundedRectangle(cornerRadius: 25)
+                                .transition(AnyTransition.slide.combined(with: .opacity).animation(.easeInOut(duration: 2.0)))
+                                .padding(.top, 50)
+                                .padding(.horizontal, 10)
+                        }
+                        VStack {
+                            HStack {
+                                TextField("search city here", text: $searchText)
+                               
+                           
+                                
+                            }
+                            .padding()
+                           
+                            .background(Color(#colorLiteral(red: 0.9999127984, green: 1, blue: 0.9998814464, alpha: 1)))
+                            .cornerRadius(17)
+                            .frame(width: UIScreen.main.bounds.width - 60, height: 100)
+                            
+                        
+                            
+                           
+                            ScrollView {
+                                ForEach(cities, id: \.self) { city in
+                                        HStack {
+                                            Text("\(city), ")
+                                                .foregroundColor(.white)
+                                                .fontWeight(.bold)
+                                                
+                                                .padding(.leading, 40)
+                                            Text("United States")
+                                                .foregroundColor(.white)
+                                            Spacer()
+                                        }
+                                        .padding(.bottom, 70)
+                                        
+                                        Spacer()
+                                    
+                                    
+                                
+                                
+                                }.padding(.top, 30)
+                            }
+                            Spacer()
+                        }.padding(.top, 50)
+                }
+                
+        
+            }
+                
+                    HStack {
+                        Spacer()
+                        
+                        Button(action: {
+                            
+                            addButtonTapped.toggle()
+                            
+                        }) {
+                            Circle()
+                                .fill(addButtonTapped ? .black : Color.white)
+                                .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/)
+                                .padding(.horizontal)
+                                .shadow(color: Color(addButtonTapped ? .black : #colorLiteral(red: 0.7450370193, green: 0.7255315781, blue: 0.7254028916, alpha: 1)), radius: 4, x: 0, y: 0)
+                                .overlay(
+                                    Image(systemName: "xmark")
+                                        .foregroundColor(Color( addButtonTapped ? .white : #colorLiteral(red: 0.7450370193, green: 0.7255315781, blue: 0.7254028916, alpha: 1)) )
+                                        .font(.system(size: 35, weight: .bold, design: .rounded))
+                                        
+                                        .rotationEffect(.degrees(addButtonTapped ? 0 : 135))
+                                )
+                                .animation(.easeInOut)
+                        }
+                        
+                    }
+            
+            
+            
+        }
+        
+    }
     }
 }
 
